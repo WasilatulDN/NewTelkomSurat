81e3415b171d8fa4a7f2ec8093c4385e4e9963e9
wasilatul <wasilatuldewi@gmail.com>
2020-01-14 14:30:46 +0700

    export tabel

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -324,262 +324,6 @@ class AdminController extends Controller
 
     }
 
-    public function exportAction()
-    {
-        //Declaring css
-        $header_css = "PUT Your CSS if needed";
-        $data_css = "PUT Your CSS if needed";
-        //If data comes from model then
-        $data = nomor_surat::find();
-        //As i'm using join query i feel good using createBuilder
-        // $data = $this->modelsManager->createBuilder()
-        //       ->columns("comment.comment, comment.username, comment.email, comment.postedat,item.name,item.photo,item.view,item.categoryid,item.id")
-        //       ->From('comment')
-        //       ->innerjoin('item', 'comment.productid = item.id')
-        //       ->where("comment.email = 'pass your value here' ")
-        //       ->getQuery()
-        //       ->execute(); 
-
-        $table = "<table>
-        <tr>
-        <td style='$header_css'>No</td>
-        <td style='$header_css'>Nama Pengupload</td>
-        <td style='$header_css'>Nama Surat</td>
-        <td style='$header_css'>Jenis Surat</td>
-        <td style='$header_css'>Nomor Surat</td>
-        <td style='$header_css'>Tanggal</td>
-        <td style='$header_css'>Status Verifikasi</td>
-        <td style='$header_css'>Setuju/Tidak</td>
-        </tr>";
-        foreach ($data as $row) {
-            if($row->jenis_surat == 1)
-            {
-                $jenissurat = "Berita Acara Penjelasan";
-            }
-            elseif($row->jenis_surat == 2)
-            {
-                $jenissurat = "BASO";
-            }
-            elseif($row->jenis_surat == 3)
-            {
-                $jenissurat = "BADO";
-            }
-            elseif($row->jenis_surat == 4)
-            {
-                $jenissurat = "Surat Keluar";
-            }
-            elseif($row->jenis_surat == 5)
-            {
-                $jenissurat = "P0/P1";
-            }
-            elseif($row->jenis_surat == 6)
-            {
-                $jenissurat = "Surat Penawaran";
-            }
-
-
-            if($row->pengecekan == 1)
-            {
-                $verifikasi = "Terverifikasi";
-            }
-            elseif($row->pengecekan == -1)
-            {
-                $verifikasi = "Ditolak";
-            }
-            else
-            {
-                $verifikasi = "Belum Verifikasi";
-            }
-
-            $table.= "<tr>
-            <td style='$data_css'>$row->id</td>
-            <td style='$data_css'>$row->name</td>
-            <td style='$data_css'>$row->nama_surat</td>
-            <td style='$data_css'>$jenissurat</td>
-            <td style='$data_css'>$row->no_surat</td>
-            <td style='$data_css'>$row->tanggal</td>
-            <td style='$data_css'>$row->nama_pengupload</td>
-            <td style='$data_css'>$verifikasi</td>
-            </tr>";
-        }
-        $table.= '</table>';
-
-        header ("Content-type: application/xls;charset=UTF-8");
-        header ("Content-Disposition: attachment; filename=DataSurat.xls" );
-        return $table;
-    }
-
-    public function exportSudahAction()
-    {
-        //Declaring css
-        $header_css = "PUT Your CSS if needed";
-        $data_css = "PUT Your CSS if needed";
-        //If data comes from model then
-        $data = nomor_surat::find(["file is not NULL"]);
-        //As i'm using join query i feel good using createBuilder
-        // $data = $this->modelsManager->createBuilder()
-        //       ->columns("comment.comment, comment.username, comment.email, comment.postedat,item.name,item.photo,item.view,item.categoryid,item.id")
-        //       ->From('comment')
-        //       ->innerjoin('item', 'comment.productid = item.id')
-        //       ->where("comment.email = 'pass your value here' ")
-        //       ->getQuery()
-        //       ->execute(); 
-
-        $table = "<table>
-        <tr>
-        <td style='$header_css'>No</td>
-        <td style='$header_css'>Nama Pengupload</td>
-        <td style='$header_css'>Nama Surat</td>
-        <td style='$header_css'>Jenis Surat</td>
-        <td style='$header_css'>Nomor Surat</td>
-        <td style='$header_css'>Tanggal</td>
-        <td style='$header_css'>Nama Pengupload</td>
-        <td style='$header_css'>Status Verifikasi</td>
-        </tr>";
-        foreach ($data as $row) {
-            if($row->jenis_surat == 1)
-            {
-                $jenissurat = "Berita Acara Penjelasan";
-            }
-            elseif($row->jenis_surat == 2)
-            {
-                $jenissurat = "BASO";
-            }
-            elseif($row->jenis_surat == 3)
-            {
-                $jenissurat = "BADO";
-            }
-            elseif($row->jenis_surat == 4)
-            {
-                $jenissurat = "Surat Keluar";
-            }
-            elseif($row->jenis_surat == 5)
-            {
-                $jenissurat = "P0/P1";
-            }
-            elseif($row->jenis_surat == 6)
-            {
-                $jenissurat = "Surat Penawaran";
-            }
-
-            if($row->pengecekan == 1)
-            {
-                $verifikasi = "Terverifikasi";
-            }
-            elseif($row->pengecekan == -1)
-            {
-                $verifikasi = "Ditolak";
-            }
-            else
-            {
-                $verifikasi = "Belum Verifikasi";
-            }
-
-
-            $table.= "<tr>
-            <td style='$data_css'>$row->id</td>
-            <td style='$data_css'>$row->name</td>
-            <td style='$data_css'>$row->nama_surat</td>
-            <td style='$data_css'>$jenissurat</td>
-            <td style='$data_css'>$row->no_surat</td>
-            <td style='$data_css'>$row->tanggal</td>
-            <td style='$data_css'>$row->nama_pengupload</td>
-            <td style='$data_css'>$verifikasi</td>
-            </tr>";
-        }
-        $table.= '</table>';
-
-        header ("Content-type: application/xls;charset=UTF-8");
-        header ("Content-Disposition: attachment; filename=SuratSudahUpload.xls" );
-        return $table;
-    }
-
-    public function exportBelumAction()
-    {
-        //Declaring css
-        $header_css = "PUT Your CSS if needed";
-        $data_css = "PUT Your CSS if needed";
-        //If data comes from model then
-        $data = nomor_surat::find(["file is NULL"]);
-        //As i'm using join query i feel good using createBuilder
-        // $data = $this->modelsManager->createBuilder()
-        //       ->columns("comment.comment, comment.username, comment.email, comment.postedat,item.name,item.photo,item.view,item.categoryid,item.id")
-        //       ->From('comment')
-        //       ->innerjoin('item', 'comment.productid = item.id')
-        //       ->where("comment.email = 'pass your value here' ")
-        //       ->getQuery()
-        //       ->execute(); 
-
-        $table = "<table>
-        <tr>
-        <td style='$header_css'>No</td>
-        <td style='$header_css'>Nama Pengupload</td>
-        <td style='$header_css'>Nama Surat</td>
-        <td style='$header_css'>Jenis Surat</td>
-        <td style='$header_css'>Nomor Surat</td>
-        <td style='$header_css'>Tanggal</td>
-        <td style='$header_css'>Nama Pengupload</td>
-        <td style='$header_css'>Status Verifikasi</td>
-        </tr>";
-        foreach ($data as $row) {
-            if($row->jenis_surat == 1)
-            {
-                $jenissurat = "Berita Acara Penjelasan";
-            }
-            elseif($row->jenis_surat == 2)
-            {
-                $jenissurat = "BASO";
-            }
-            elseif($row->jenis_surat == 3)
-            {
-                $jenissurat = "BADO";
-            }
-            elseif($row->jenis_surat == 4)
-            {
-                $jenissurat = "Surat Keluar";
-            }
-            elseif($row->jenis_surat == 5)
-            {
-                $jenissurat = "P0/P1";
-            }
-            elseif($row->jenis_surat == 6)
-            {
-                $jenissurat = "Surat Penawaran";
-            }
-
-            if($row->pengecekan == 1)
-            {
-                $verifikasi = "Terverifikasi";
-            }
-            elseif($row->pengecekan == -1)
-            {
-                $verifikasi = "Ditolak";
-            }
-            else
-            {
-                $verifikasi = "Belum Verifikasi";
-            }
-
-
-            $table.= "<tr>
-            <td style='$data_css'>$row->id</td>
-            <td style='$data_css'>$row->name</td>
-            <td style='$data_css'>$row->nama_surat</td>
-            <td style='$data_css'>$jenissurat</td>
-            <td style='$data_css'>$row->no_surat</td>
-            <td style='$data_css'>$row->tanggal</td>
-            <td style='$data_css'>$row->nama_pengupload</td>
-            <td style='$data_css'>$verifikasi</td>
-            </tr>";
-        }
-        $table.= '</table>';
-
-        header ("Content-type: application/xls;charset=UTF-8");
-        header ("Content-Disposition: attachment; filename=SuratBelumUpload.xls" );
-        return $table;
-    }
-
-
     public function verifdetailAction($id)
     {   
         $ids = $this->session->get('admin')['tipe'];
@@ -647,4 +391,240 @@ class AdminController extends Controller
         }
 
     }
+
+    public function exportAction()
+    {
+        //Declaring css
+        $header_css = "PUT Your CSS if needed";
+        $data_css = "PUT Your CSS if needed";
+        //If data comes from model then
+        $data = $this->modelsManager->createBuilder()
+        ->columns("nomor_surat.id, nomor_surat.name, nomor_surat.nama_surat, nomor_surat.no_surat, nomor_surat.tanggal, nomor_surat.nama_pengupload, nomor_surat.pengecekan, jenis_surat.nama_surat as namajenis, nomor_surat.file")
+        ->From('nomor_surat')
+        ->innerjoin('jenis_surat', 'nomor_surat.jenis_surat = jenis_surat.id')
+        // ->where(" file is not NULL ")
+        ->getQuery()
+        ->execute(); 
+        //As i'm using join query i feel good using createBuilder
+        // $data = $this->modelsManager->createBuilder()
+        //       ->columns("comment.comment, comment.username, comment.email, comment.postedat,item.name,item.photo,item.view,item.categoryid,item.id")
+        //       ->From('comment')
+        //       ->innerjoin('item', 'comment.productid = item.id')
+        //       ->where("comment.email = 'pass your value here' ")
+        //       ->getQuery()
+        //       ->execute(); 
+
+        $table = "<table>
+        <tr>
+        <td style='$header_css'>No</td>
+        <td style='$header_css'>Nama Pengupload</td>
+        <td style='$header_css'>Nama Surat</td>
+        <td style='$header_css'>Jenis Surat</td>
+        <td style='$header_css'>Nomor Surat</td>
+        <td style='$header_css'>Tanggal</td>
+        <td style='$header_css'>Nama Pengupload</td>
+        <td style='$header_css'>File</td>
+        <td style='$header_css'>Status Verifikasi</td>
+        </tr>";
+
+        $no = 1;
+        foreach ($data as $row) {
+
+            if($row->pengecekan == 1)
+            {
+                $verifikasi = "Terverifikasi";
+            }
+            elseif($row->pengecekan == -1)
+            {
+                $verifikasi = "Ditolak";
+            }
+            else
+            {
+                $verifikasi = "Belum Verifikasi";
+            }
+
+            if($row->file == NULL){
+                $file = "Belum Upload";
+            }
+            elseif($row->file != NULL){
+                $file = "Sudah Upload";
+            }
+
+            $table.= "<tr>
+            <td style='$data_css'>$no</td>
+            <td style='$data_css'>$row->name</td>
+            <td style='$data_css'>$row->nama_surat</td>
+            <td style='$data_css'>$row->namajenis</td>
+            <td style='$data_css'>$row->no_surat</td>
+            <td style='$data_css'>$row->tanggal</td>
+            <td style='$data_css'>$row->nama_pengupload</td>
+            <td style='$data_css'>$file</td>
+            <td style='$data_css'>$verifikasi</td>
+            </tr>";
+            $no ++;
+        }
+        $table.= '</table>';
+
+        header ("Content-type: application/xls;charset=UTF-8");
+        header ("Content-Disposition: attachment; filename=DataSurat.xls" );
+        return $table;
+    }
+
+    public function exportSudahAction()
+    {
+        //Declaring css
+        $header_css = "PUT Your CSS if needed";
+        $data_css = "PUT Your CSS if needed";
+        $data = $this->modelsManager->createBuilder()
+        ->columns("nomor_surat.id, nomor_surat.name, nomor_surat.nama_surat, nomor_surat.no_surat, nomor_surat.tanggal, nomor_surat.nama_pengupload, nomor_surat.pengecekan, jenis_surat.nama_surat as namajenis, nomor_surat.file")
+        ->From('nomor_surat')
+        ->innerjoin('jenis_surat', 'nomor_surat.jenis_surat = jenis_surat.id')
+        ->where(" file is not NULL ")
+        ->getQuery()
+        ->execute(); 
+        //As i'm using join query i feel good using createBuilder
+        // $data = $this->modelsManager->createBuilder()
+        //       ->columns("comment.comment, comment.username, comment.email, comment.postedat,item.name,item.photo,item.view,item.categoryid,item.id")
+        //       ->From('comment')
+        //       ->innerjoin('item', 'comment.productid = item.id')
+        //       ->where("comment.email = 'pass your value here' ")
+        //       ->getQuery()
+        //       ->execute(); 
+
+        $table = "<table>
+        <tr>
+        <td style='$header_css'>No</td>
+        <td style='$header_css'>Nama Pengupload</td>
+        <td style='$header_css'>Nama Surat</td>
+        <td style='$header_css'>Jenis Surat</td>
+        <td style='$header_css'>Nomor Surat</td>
+        <td style='$header_css'>Tanggal</td>
+        <td style='$header_css'>Nama Pengupload</td>
+        <td style='$header_css'>File</td>
+        <td style='$header_css'>Status Verifikasi</td>
+        </tr>";
+
+        $no = 1;
+        foreach ($data as $row) {
+
+            if($row->pengecekan == 1)
+            {
+                $verifikasi = "Terverifikasi";
+            }
+            elseif($row->pengecekan == -1)
+            {
+                $verifikasi = "Ditolak";
+            }
+            else
+            {
+                $verifikasi = "Belum Verifikasi";
+            }
+
+
+            if($row->file == NULL){
+                $file = "Belum Upload";
+            }
+            elseif($row->file != NULL){
+                $file = "Sudah Upload";
+            }
+
+            $table.= "<tr>
+            <td style='$data_css'>$no</td>
+            <td style='$data_css'>$row->name</td>
+            <td style='$data_css'>$row->nama_surat</td>
+            <td style='$data_css'>$row->namajenis</td>
+            <td style='$data_css'>$row->no_surat</td>
+            <td style='$data_css'>$row->tanggal</td>
+            <td style='$data_css'>$row->nama_pengupload</td>
+            <td style='$data_css'>$file</td>
+            <td style='$data_css'>$verifikasi</td>
+            </tr>";
+
+            $no++;
+        }
+        $table.= '</table>';
+
+        header ("Content-type: application/xls;charset=UTF-8");
+        header ("Content-Disposition: attachment; filename=SuratSudahUpload.xls" );
+        return $table;
+    }
+
+    public function exportBelumAction()
+    {
+        //Declaring css
+        $header_css = "PUT Your CSS if needed";
+        $data_css = "PUT Your CSS if needed";
+        //If data comes from model then
+        $data = $this->modelsManager->createBuilder()
+        ->columns("nomor_surat.id, nomor_surat.name, nomor_surat.nama_surat, nomor_surat.no_surat, nomor_surat.tanggal, nomor_surat.nama_pengupload, nomor_surat.pengecekan, jenis_surat.nama_surat as namajenis, nomor_surat.file")
+        ->From('nomor_surat')
+        ->innerjoin('jenis_surat', 'nomor_surat.jenis_surat = jenis_surat.id')
+        ->where(" file is NULL ")
+        ->getQuery()
+        ->execute(); 
+        //As i'm using join query i feel good using createBuilder
+        // $data = $this->modelsManager->createBuilder()
+        //       ->columns("comment.comment, comment.username, comment.email, comment.postedat,item.name,item.photo,item.view,item.categoryid,item.id")
+        //       ->From('comment')
+        //       ->innerjoin('item', 'comment.productid = item.id')
+        //       ->where("comment.email = 'pass your value here' ")
+        //       ->getQuery()
+        //       ->execute(); 
+
+        $table = "<table>
+        <tr>
+        <td style='$header_css'>No</td>
+        <td style='$header_css'>Nama Pengupload</td>
+        <td style='$header_css'>Nama Surat</td>
+        <td style='$header_css'>Jenis Surat</td>
+        <td style='$header_css'>Nomor Surat</td>
+        <td style='$header_css'>Tanggal</td>
+        <td style='$header_css'>Nama Pengupload</td>
+        <td style='$header_css'>File</td>
+        <td style='$header_css'>Status Verifikasi</td>
+        </tr>";
+
+        $no=1;
+        foreach ($data as $row) {
+
+            if($row->pengecekan == 1)
+            {
+                $verifikasi = "Terverifikasi";
+            }
+            elseif($row->pengecekan == -1)
+            {
+                $verifikasi = "Ditolak";
+            }
+            else
+            {
+                $verifikasi = "Belum Verifikasi";
+            }
+            if($row->file == NULL){
+                $file = "Belum Upload";
+            }
+            elseif($row->file != NULL){
+                $file = "Sudah Upload";
+            }
+
+            $table.= "<tr>
+            <td style='$data_css'>$no</td>
+            <td style='$data_css'>$row->name</td>
+            <td style='$data_css'>$row->nama_surat</td>
+            <td style='$data_css'>$row->namajenis</td>
+            <td style='$data_css'>$row->no_surat</td>
+            <td style='$data_css'>$row->tanggal</td>
+            <td style='$data_css'>$row->nama_pengupload</td>
+            <td style='$data_css'>$file</td>
+            <td style='$data_css'>$verifikasi</td>
+            </tr>";
+
+            $no++;
+        }
+        $table.= '</table>';
+
+        header ("Content-type: application/xls;charset=UTF-8");
+        header ("Content-Disposition: attachment; filename=SuratBelumUpload.xls" );
+        return $table;
+    }
+
 }
